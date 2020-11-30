9d35850585ac1cb966a7290f490e2bcda146de76
sjvasconcello <sjvasconcello@gmail.com>
2020-11-30 13:11:33 -0300

    :construction: Sign In added
    Platzi Django

--- django/platzigram/users/views.py
+++ django/platzigram/users/views.py
@@ -3,8 +3,14 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-# Create your views here.
+# Exception
+from django.db.utils import IntegrityError
 
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+# views here.
 
 def login_view(request):
     """Login view."""
@@ -20,6 +26,34 @@ def login_view(request):
             return render(request, "users/login.html", {"error": "Invalid username or password"})
 
     return render(request, "users/login.html")
+
+def signup(request):
+    """Sign up views."""
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+
+        if password != password_confirmation:
+            return render(request, "users/signup.html", {"error": "Password confirmation does not match"})
+        
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, "users/signup.html", {"error": "User isalready used"})
+
+        user = User.objects.create_user(username=username, password=password)
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, "users/signup.html")
 
 @login_required
 def logout_view(request):
