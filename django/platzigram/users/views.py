e9b112e8b1f345a1d6bacf1fb87e1b8753a7d53e
sjvasconcello <sjvasconcello@gmail.com>
2020-12-01 23:32:50 -0300

    :construction: Working in Middlewares
    Django Plazti

--- django/platzigram/users/views.py
+++ django/platzigram/users/views.py
@@ -12,6 +12,10 @@ from users.models import Profile
 
 # views here.
 
+def update_profile(request):
+    """Update a user's profile view."""
+    return render(request, "users/update_profile.html")
+
 def login_view(request):
     """Login view."""
 
