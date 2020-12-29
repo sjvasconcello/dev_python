d9e87f013b155fa41685ffa21b7e7cdaa1299d87
sjvasconcello <sjvasconcello@gmail.com>
2020-12-29 16:57:34 -0300

    :construction: Protecting the profile view
    Django Platzi v.29

--- django/platzigram/posts/urls.py
+++ django/platzigram/posts/urls.py
@@ -7,7 +7,7 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path(route="", view=views.list_posts, name="feed"),
+    path(route="", view=views.PostsFeedView.as_view(), name="feed"),
     path(route="posts/new", view=views.create_post, name="create"),
 ]
 
--- django/platzigram/users/views.py
+++ django/platzigram/users/views.py
@@ -1,12 +1,36 @@
 # Django
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView
 from django.shortcuts import render, redirect
+from django.urls import reverse
+
+# Models
+from django.contrib.auth.models import User
+from posts.models import Post
 
 # Forms
 from users.forms import ProfileForm, SignupForm
 
 
+class UserDetailView(LoginRequiredMixin ,DetailView):
+    """User detail view."""
+    template_name = "users/detail.html"
+    slug_field = "username"
+    slug_url_kwarg = "username"
+    queryset = User.objects.all()
+    context_object_name = "user"
+
+    def get_context_data(self, **kwargs):
+        """Add user's posts to context."""
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        context["posts"] = Post.objects.filter(user=user).order_by("-created")
+        return context
+
+
+@login_required
 def update_profile(request):
     """Update a user's profile view."""
 
@@ -21,15 +45,17 @@ def update_profile(request):
             profile.picture = data["picture"]
             profile.biography = data["biography"]
             profile.save()
-            
-            return redirect("update_profile")
+
+            url = reverse("users:detail", kwargs={
+                          "username": request.user.username})
+            return redirect(url)
     else:
         form = ProfileForm()
 
     return render(
-        request = request,
-        template_name = "users/update_profile.html",
-        context = {
+        request=request,
+        template_name="users/update_profile.html",
+        context={
             "profile": profile,
             "user": request.user,
             "form": form
@@ -45,7 +71,7 @@ def login_view(request):
         user = authenticate(request, password=password, username=username)
         if user:
             login(request, user)
-            return redirect("feed")
+            return redirect("posts:feed")
         else:
             return render(request, "users/login.html", {"error": "Invalid username or password"})
 
@@ -58,19 +84,19 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("login")
-    else: 
+            return redirect("users:login")
+    else:
         form = SignupForm()
-    
+
     return render(
-        request =  request,
-        template_name = "users/signup.html",
-        context = {"form": form}
+        request=request,
+        template_name="users/signup.html",
+        context={"form": form}
     )
-        
+
 
 @login_required
 def logout_view(request):
     """Logout a user"""
     logout(request)
-    return redirect("logout")
+    return redirect("users:login")
