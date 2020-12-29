7323b36e381e952c6e8aefadaaa973e7beb315e6
sjvasconcello <sjvasconcello@gmail.com>
2020-12-29 13:25:21 -0300

    :construction: Class based views - Django
    Django Platzi v.28

---
+++ django/platzigram/posts/urls.py
@@ -0,0 +1,16 @@
+"""Post URLs."""
+
+# Django 
+from django.urls import path
+
+# Views
+from posts import views
+
+urlpatterns = [
+    path(route="", view=views.list_posts, name="feed"),
+    path(route="posts/new", view=views.create_post, name="create"),
+]
+
+
+
+
