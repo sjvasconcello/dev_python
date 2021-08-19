736545a6f38678b88790b04be38b3e8bac0759c1
sjvasconcello <sjvasconcello@gmail.com>
2021-08-19 19:01:56 -0400

    :tada: Continue with Flask

--- platzi_flask/main.py
+++ platzi_flask/main.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 # En este caso __name__ es main.py
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 todos = ["TODO1", "TODO2", "TODO3"]
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html", error=error)
 
 @app.route("/")
 def index():
