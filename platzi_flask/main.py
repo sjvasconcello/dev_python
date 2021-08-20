0f4359f619a5dc5cb9e505a80dc820d9d0d18934
sjvasconcello <sjvasconcello@gmail.com>
2021-08-19 23:02:48 -0400

    :construction: Flask v.19

--- platzi_flask/main.py
+++ platzi_flask/main.py
@@ -1,30 +1,56 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 # En este caso __name__ es main.py
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+app.config["SECRET_KEY"] = "SUPER SECRETO"
+
 todos = ["TODO1", "TODO2", "TODO3"]
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Nombre de usuario", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Enviar")
+
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template("404.html", error=error)
 
+
 @app.route("/")
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect("/hello"))
-    response.set_cookie("user_ip", user_ip)
+    session["user_ip"] = user_ip
 
     return response
 
 
-@app.route("/hello")
+@app.route("/hello", methods=["GET", "POST"])
 def hello():
-    user_ip = request.cookies.get("user_ip")
+    user_ip = session.get("user_ip")
+    login_form = LoginForm()
+    username = session.get("username")
     context = {
-        "user_ip":user_ip, "todos":todos
+        "user_ip": user_ip,
+        "todos": todos,
+        "login_form": login_form,
+        "username": username
     }
 
-    return render_template("hello.html",**context)
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session["username"] = username
+
+        flash("Nombre de usuario registrado con exito")
+
+        return redirect(url_for("index"))
+
+    return render_template("hello.html", **context)
--- platzi_flask/run.sh
+++ platzi_flask/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#source venv/bin/activate
-#pip install -r requierements.txt
+source venv/bin/activate
+pip install -r requierements.txt
 
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
