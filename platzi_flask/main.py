3ee1af3469e897969a2974aa6f057959e1ecaa54
sjvasconcello <sjvasconcello@gmail.com>
2021-09-02 18:53:35 -0400

    :construction: Blueprints II
    Flask Platzi Video 24

--- platzi_flask/main.py
+++ platzi_flask/main.py
@@ -1,61 +1,51 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
 
-# En este caso __name__ es main.py
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+from app import create_app
+from app.forms import LoginForm
 
-app.config["SECRET_KEY"] = "SUPER SECRETO"
+app = create_app()
 
-todos = ["TODO1", "TODO2", "TODO3"]
+
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
+
 
 @app.cli.command()
 def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner().run(tests)
 
-class LoginForm(FlaskForm):
-    username = StringField("Nombre de usuario", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Enviar")
-
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template("404.html", error=error)
+    return render_template('404.html', error=error)
 
 
-@app.route("/")
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
+
+@app.route('/')
 def index():
     user_ip = request.remote_addr
-    response = make_response(redirect("/hello"))
-    session["user_ip"] = user_ip
+
+    response = make_response(redirect('/hello'))
+    session['user_ip'] = user_ip
 
     return response
 
 
-@app.route("/hello", methods=["GET", "POST"])
+@app.route('/hello', methods=['GET'])
 def hello():
-    user_ip = session.get("user_ip")
-    login_form = LoginForm()
-    username = session.get("username")
+    user_ip = session.get('user_ip')
+    username = session.get('username')
+
     context = {
-        "user_ip": user_ip,
-        "todos": todos,
-        "login_form": login_form,
-        "username": username
+        'user_ip': user_ip,
+        'todos': todos,
+        'username': username
     }
 
-    if login_form.validate_on_submit():
-        username = login_form.username.data
-        session["username"] = username
+    return render_template('hello.html', **context)
 
-        flash("Nombre de usuario registrado con exito")
-
-        return redirect(url_for("index"))
-
-    return render_template("hello.html", **context)
